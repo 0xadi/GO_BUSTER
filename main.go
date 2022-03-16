0d9b4711a62985986bacffd7f42bcc340db5bcf9
0xadi <101038537+0xadi@users.noreply.github.com>
2022-03-16 20:17:22 +0530

    Add files via upload

---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+func main() {
+	//words := readwordlist("/home/ninja/Downloads/SecLists/Discovery/Web-Content/directory-list-2.3-small.txt")
+	words := readwordlist("wordlist.txt")
+	print(words)
+
+}
